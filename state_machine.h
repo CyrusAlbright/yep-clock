8f11e34b2a6dd0699ffa722ba1f72c7dd346c47f
Drew Christensen <emoryandrewchristensen@gmail.com>
2024-04-24 18:05:10 -0600

    Comment code, add models and circuit diagram

--- state_machine.h
+++ state_machine.h
@@ -1,3 +1,29 @@
+/*
+	A custom state machine implementation that handles scheduling and manual PWM.
+
+	Inputs:
+		now:
+			Current timestamp
+		unit_difference:
+			-1 if it needs to move backwards, 1 if it needs to move forwards, 0 otherwise
+
+	All state transitions:
+		If it is in the Idle state and it needs to move:
+			It will start moving by switching to the PulseOn state and scheduling the next state transition
+		If it is in the PulseOn state and the next state transition is now:
+			It will switch to PulseOff and schedule the next state transition, in addition to adjusting the current step
+		If it is in the PulseOff state and it no longer needs to move:
+			It will switch back to the Idle state
+		If it is in the PulseOff state and its next state transition is now:
+			It will switch back to the PulseOn state and schedule the next state transition
+
+	Outputs:
+		activate_coils:
+			Whether or not to activate the coils this instant
+		steps_increment:
+			Adjusts the current step on a state transition from PulseOn to PulseOff
+*/
+
 #pragma once
 
 #include <stdbool.h>
--- utils.h
+++ utils.h
@@ -4,8 +4,15 @@
 
 typedef uint32_t Time;
 
+// The % operator has strange behavior with negative inputs
+// This mod function compensates for negatives
 int32_t mod(int32_t a, int32_t b);
 
+// Get the smallest difference (normalized to -half_period and +half_period)
+// between a source and target angle in modular arithmetic
 int32_t angle_diff(int32_t source, int32_t target, int32_t period);
 
+// Return 1 if input is positive
+// 0 if input is zero
+// -1 if input is negative
 int32_t unit_direction(int32_t);
